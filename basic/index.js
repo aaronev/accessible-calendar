cea9ca07b179ea0d0c9aed3b2d1d8b5dd06361ad
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-03-16 16:19:25 -0700

    adds event listner to button

--- basic/index.js
+++ basic/index.js
@@ -1,7 +1,14 @@
 console.log('working')
 
+document.querySelector('button').addEventListener('click', activate);
+
+
+function activate() {
+  new AccessibleCalendar
+}
+
 class AccessibleCalendar {
   constructor() {
-    
+
   }
 }
