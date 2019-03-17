a6ed178bd95ef1ed93647b986769f0d17c412ccc
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-03-16 17:14:39 -0700

    adds grid class and calendar class

--- basic/index.js
+++ basic/index.js
@@ -1,15 +1,48 @@
 console.log('working')
 
-document.querySelector('button').addEventListener('click', activate);
+document.querySelector('button').addEventListener('click', event => new AccessibleCalendar(event));
 
+class Grid {
+  construcor() {
+    this.grid = document.createElement('div');
+    this.grid.setAttribute('role', 'grid');
+  }
 
-function activate() {
-  console.log('activation works')
-  new AccessibleCalendar;
+  columnHeaders(array) {
+    var row = document.createElement('div');
+    row.setAttribute('role', 'row');
+    array.forEach((txt) => {  
+      var cell = document.createElement('span');
+      cell.setAttribute('role', 'columnheader')
+      cell.innerText = 'txt';
+      row.appendChild(cell);
+    }
+    this.grid.appendChild(row);
+  } 
+
+  addRow(array) {
+    var row = document.createElement('div');
+    row.setAttribute('role', 'row');
+    array.forEach((txt) => {  
+      var cell = document.createElement('span');
+      cell.setAttribute('role', 'cell')
+      cell.innerText = 'txt';
+      row.appendChild(cell);
+    }
+    this.grid.appendChild(row);
+  }
+
+  appendTo(query) {
+    document.querySelector(query).appendChild(this.grid)
+  }
 }
 
 class AccessibleCalendar {
-  constructor() {
-    
+  constructor(event) {
+    console.log('es6+ is working', event);
+    this.calendar = document.createElement('div');
+    this.grid = new Grid;
   }
+
+
 }
