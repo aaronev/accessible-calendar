713465e6d85919ae2343ee02244468a8eed0c014
Aaron Villanueva <aarone.villanueva@gmail.com>
2019-03-16 21:21:28 -0700

    fixes appending

--- basic/index.js
+++ basic/index.js
@@ -1,7 +1,8 @@
 'use strict';
-console.log('working');
 
-document.querySelector('button').addEventListener('click', event => new AccessibleCalendar().append());
+const moment = require('moment');
+
+document.querySelector('button').addEventListener('click', event => new AccessibleCalendar);
 
 class Grid {
   constructor() {
@@ -19,7 +20,7 @@ class Grid {
       row.appendChild(cell);
     });
     this.grid.appendChild(row);
-  } 
+  };
 
   addRow(array) {
     const row = document.createElement('div');
@@ -31,10 +32,10 @@ class Grid {
       row.appendChild(cell);
     });
     this.grid.appendChild(row);
-  }
+  };
 
-  appendTo(query) {
-    query.appendChild(this.grid)
+  getGrid() {
+    return this.grid;
   }
 }
 
@@ -43,13 +44,12 @@ class AccessibleCalendar {
     this.calendar = document.createElement('div');
     this.grid = new Grid();
     this.years = (() => {
-      const generatedYears = []
+      const generatedYears = [];
       for(let i = 1991; i < 20; i++) {
-        generatedYears.push(i)
+        generatedYears.push(i);
       }
-      return  generatedYears
+      return generatedYears;
     })();
-    this.days = this.grid.columnHeaders(['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']);
     this.months = [
     'January', 
     'February', 
@@ -64,16 +64,13 @@ class AccessibleCalendar {
     'November', 
     'December'
     ];
-  };
-
-  append(){
+    this.grid.columnHeaders(['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']);
     this.grid.addRow(['',1,2,3,4,5,6]);
     this.grid.addRow([7,8,9,10,11,12,13]);
     this.grid.addRow([14,15,16,17,18,19,20]);
     this.grid.addRow([21,22,23,24,25,26,27]);
     this.grid.addRow([28,29,30,31,'','','']);
-    this.grid.appendTo(this.calendar);
+    this.calendar.appendChild(this.grid.getGrid());
     document.body.appendChild(this.calendar);
   };
-
 }
